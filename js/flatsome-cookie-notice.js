81528058df6ca773764eb4cede4001c33aeaecef
atracion20 <atracion426@gmail.com>
2024-08-23 12:17:27 -0300

    Add files via upload

---
+++ js/flatsome-cookie-notice.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function(){'use strict'
+var $notice=jQuery('.flatsome-cookies')
+var cookieId='flatsome_cookie_notice'
+var cookieValue=flatsomeVars.options.cookie_notice_version
+if(cookie(cookieId)!==cookieValue){setTimeout(function(){$notice.addClass('flatsome-cookies--active')
+$notice.on('click','.flatsome-cookies__accept-btn',function(e){e.preventDefault()
+$notice.removeClass('flatsome-cookies--active').addClass('flatsome-cookies--inactive')
+cookie(cookieId,cookieValue,365)})},2500)}})
